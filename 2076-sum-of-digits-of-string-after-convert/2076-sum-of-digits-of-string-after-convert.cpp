c00aeeb8c5f1b297657a3ff05cb63ed474fd44c3
Gargayshukla <96720185+Gargayshukla@users.noreply.github.com>
2024-09-03 09:18:11 +0530

    Time: 4 ms (39.15%), Space: 7.9 MB (77.66%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     int getLucky(string s, int k) {
         
-        string ans = "";
+        int n = s.size();
 
-        //Approach - 
+        long long sum = 0;
 
-        // convert string character to number
-        // while iterate till k-- 
-        // calcute sum of digits in string
-        //convert the character string to integer and add to curr sum
-        //update the ans after each iteration
-        // return the final ans after converting final string to integer
-
-        for(auto i : s)
+        for(int i=0; i<n; i++)
         {
-            int num = i - 'a' + 1;
-            ans = ans + to_string(num);
+        int num = s[i] - 'a' + 1;
+        while(num>0)
+        {
+            sum = sum + num%10;
+            num = num/10;
         }
-
-        while(k--)
+        }
+       
+        while(k>1)
         {
-            int sum = 0;
-
-            for(auto i : ans)
+            long long newsum = 0;
+            while(sum>0)
             {
-                sum = sum + i - '0';
+                newsum = newsum + sum %10;
+                sum = sum/10;
             }
-             ans = to_string(sum);
+            sum = newsum;
+            k--;
         }
 
-        return stoi(ans);
+        return sum;
     }
 };
