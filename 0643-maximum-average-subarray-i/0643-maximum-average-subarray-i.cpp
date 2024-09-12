a7586f025e03da2c042e5f568af08a11aeb151af
Gargayshukla <96720185+Gargayshukla@users.noreply.github.com>
2024-09-12 14:40:01 +0530

    Time: 127 ms (49.34%), Space: 112.8 MB (43.31%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -3,22 +3,22 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         
         int n = nums.size();
+        
+        double avg = 0;
+        double window = 0;
 
-        double ans = 0; // storing ans
-        double window = 0; // for first window
-         
-         for(int i=0; i<k; i++)
-         {
-            window = window + nums[i];  //ans for first window
-         }
-         ans = window/k;  // avg of first window
+        for(int i=0; i<k; i++)
+        {
+            window = window + nums[i];
+        }
+        avg = window/k;
 
-         for(int right = k; right<n; right++)
-         {
-            window = window + nums[right] - nums[right - k];
-            ans = max(ans,window/k);
-         }
+        for(int right = k; right<n; right++)
+        {
+            window = window + nums[right] - nums[right-k];
+            avg = max(avg,window/k);
+        }
 
-         return ans;
+        return avg;
     }
 };
